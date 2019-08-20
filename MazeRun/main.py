4dd55f90fd9e25f2a2694ab52a28fe997199acd5
JuanIsNumberJuan <delgadojuan579@gmail.com>
2019-08-20 15:15:28 -0700

    Added MazeRun

---
+++ MazeRun/main.py
@@ -0,0 +1,25 @@
+import webapp2
+import jinja2
+import os
+
+
+jinja_env = jinja2.Environment(
+    loader = jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions = ['jinja2.ext.autoescape'],
+    autoescape=True
+)
+
+class MainMazePageHandler(webapp2.RequestHandler):
+    def get(self):
+        results_template = jinja_env.get_template('MazeHtml/maze.html')
+        self.response.write(results_template.render())
+
+
+
+
+
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainMazePageHandler)
+], debug=True)
