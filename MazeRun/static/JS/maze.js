9ae0f0a07f09d56923c9b0a572c91297ea5fcf34
marianoamenta <mariano.armenta@icloud.com>
2019-08-23 10:04:02 -0700

    timer

--- MazeRun/static/JS/maze.js
+++ MazeRun/static/JS/maze.js
@@ -41,3 +41,27 @@ function moveDown(){
 window.onload=init;
 
 window.onFrame = ()=>{console.log("hi")}
+
+var h2= document.getElementById('oof')[0]
+seconds=0, minutes=0, hours=0,
+t = 0;
+
+function add() {
+    seconds++;
+    if (seconds >= 60) {
+        seconds = 0;
+        minutes++;
+        if (minutes >= 60) {
+            minutes = 0;
+            hours++;
+        }
+    }
+
+    oof.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+
+    timer();
+}
+function timer() {
+    t = setTimeout(add, 1000);
+}
+timer();
